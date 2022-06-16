c47e2a3f8456f70394b80d700cb25f1904ca8db5
Louis-Philip Shahim <louis-philipshahim@LPs-Mac.Dlink>
2022-06-16 17:16:34 +0200

    removed version

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.0"
-    }
-  }
-}
 provider "aws" {
   # removed profile
   region  = terraform.workspace == "dev" ? "eu-west-1" : "eu-west-2"

ce3b1cddfbe26f5caa01733661413be8ae42f7a7
Louis-Philip Shahim <louis-philipshahim@LPs-Mac.Dlink>
2022-06-16 17:14:34 +0200

    adding infracost with API token

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
+}
 provider "aws" {
   # removed profile
   region  = terraform.workspace == "dev" ? "eu-west-1" : "eu-west-2"

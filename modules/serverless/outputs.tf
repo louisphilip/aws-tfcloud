3515231fd08d588e1a3f52a1ea44db8fc2b8c469
Louis-Philip Shahim <louis-philipshahim@LPs-Mac.local>
2022-06-27 12:33:11 +0200

    testing module outputs

--- modules/serverless/outputs.tf
+++ modules/serverless/outputs.tf
@@ -2,3 +2,7 @@ output "queue_url" {
   description = "This output gives other users information relating to my created resources."
   value       = aws_sqs_queue.my-queue.url
 }
+
+output "lambda_function" {
+  value = module.lambda_function
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "queue_url" {
   description = "This output gives other users information relating to my created resources."
   value       = "My Queue is deployed to ${data.aws_region.current.name} and the URL is: ${module.serverless.queue_url}"
 }
+
+output "lambda_function" {
+  value = module.serverless.lambda_function
+}

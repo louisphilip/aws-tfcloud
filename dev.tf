2488cfb175b7fc6c03d8581e2d7597c360968946
Louis-Philip Shahim <louis-philipshahim@LPs-Mac.local>
2023-02-15 09:16:03 +0200

    testing prisma integration

--- dev.tf
+++ dev.tf
@@ -1,8 +1,15 @@
 module "serverless" {
+	# checkov:skip=807098449873921024_AWS_1674029894599: Tags not required at module level
   source = "./modules/serverless"
 
   region_code   = "euw1"
   env_shorthand = terraform.workspace
   queue_name    = "new-queue"
-  bucket_name   = "louisphilipshahimtestingcooliac" 
+  bucket_name   = "louisphilipshahimtestingcooliac"
+  
+}
+
+module "database" {
+  source        = "./modules/database"
+  db_identifier = "testing-instance"
 }
--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   # removed profile
-  region  = terraform.workspace == "dev" ? "eu-west-1" : "eu-west-2"
+  region = terraform.workspace == "dev" ? "eu-west-1" : "eu-west-2"
   default_tags {
     tags = {
       Environment : terraform.workspace == "dev" ? "Development" : "Production",

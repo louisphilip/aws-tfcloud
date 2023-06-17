802db0f1266a2e347b52e5a378a48a304978b861
Louis-Philip Shahim <louis-philipshahim@LPs-Mac.local>
2023-06-17 16:38:50 +0200

    applying checkov fix from vscode suggestions

--- modules/serverless/s3.tf
+++ modules/serverless/s3.tf
@@ -1,5 +1,15 @@
 resource "aws_s3_bucket" "test" {
-  # checkov:skip=807098449873921024_AWS_1681255187702: ADD REASON
-  # checkov:skip=807098449873921024_AWS_1681255187743: ADD REASON
   name = "${local.common_prefix}-${var.bucket_name}"
+}
+
+
+resource "aws_s3_bucket" "test_log_bucket" {
+  bucket = "test-log-bucket"
+}
+
+resource "aws_s3_bucket_logging" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  target_bucket = aws_s3_bucket.test_log_bucket.id
+  target_prefix = "log/"
 }

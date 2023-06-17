bbf18a4fc02aa5be2303997346e0ee77e163ee32
Louis-Philip Shahim <louis-philipshahim@LPs-Mac.local>
2023-06-17 16:36:45 +0200

    adding checkov fixes for s3 bucket

--- modules/serverless/s3.tf
+++ modules/serverless/s3.tf
@@ -1,3 +1,5 @@
 resource "aws_s3_bucket" "test" {
+  # checkov:skip=807098449873921024_AWS_1681255187702: ADD REASON
+  # checkov:skip=807098449873921024_AWS_1681255187743: ADD REASON
   name = "${local.common_prefix}-${var.bucket_name}"
 }
